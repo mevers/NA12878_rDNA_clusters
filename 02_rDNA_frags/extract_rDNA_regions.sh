ed654a32728f3aea9ce8c660abc698b9bad1058e
Maurits Evers <maurits.evers@anu.edu.au>
2020-05-19 12:39:17 +1000

    Tidied transcribed BED/FA generation rules

--- 02_rDNA_frags/extract_rDNA_regions.sh
+++ 02_rDNA_frags/extract_rDNA_regions.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+id="U13369.1"
+
+
+# Extract coding regions from annotation
+cat ../00_ref_sequences/rDNA_GenBank/${id}.bed | grep \
+    -e "\dS" -e "ETS" -e "ITS" > ${id}_transcribed.bed
+
+
+# Get coding region sequences
 bedtools getfasta \
     -name \
-	-fi ../00_ref_sequences/rDNA_GenBank/U13369.1.fa \
-	-bed U13369.1_transcribed.bed | bgzip > rDNA_transcribed.fa.gz
+	-fi ../00_ref_sequences/rDNA_GenBank/${id}.fa \
+	-bed ${id}_transcribed.bed | bgzip > rDNA_transcribed.fa.gz
