3ccc4cb19d512e4f2370b94ffca09e83d0ee63c1
Maurits Evers <maurits.evers@anu.edu.au>
2020-05-19 13:31:05 +1000

    Refactored alignment rule; added upstream flanking region to analysis

--- 02_rDNA_frags/extract_rDNA_regions.sh
+++ 02_rDNA_frags/extract_rDNA_regions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 id="U13369.1"
+flank=500
 
 
 # Extract coding regions from annotation
@@ -8,8 +9,21 @@ cat ../00_ref_sequences/rDNA_GenBank/${id}.bed | grep \
     -e "\dS" -e "ETS" -e "ITS" > ${id}_transcribed.bed
 
 
-# Get coding region sequences
+# Create upstream flank region from last line of BED file and store in
+# final BED file for the regions of interest (ROI)
+function join_by { local IFS="$1"; shift; echo "$*"; }
+last=$(tail -n 1 ../00_ref_sequences/rDNA_GenBank/${id}.bed)
+arr=(${last//\t/})
+arr[1]=$(( ${arr[2]} - $flank ))
+arr[3]="upstream_flank"
+arr[6]=${arr[1]}
+join_by $'\t' "${arr[@]}" > tmp.bed
+cat ${id}_transcribed.bed tmp.bed > ${id}_roi.bed
+rm -f tmp.bed
+
+
+# Get sequences for ROI's
 bedtools getfasta \
     -name \
 	-fi ../00_ref_sequences/rDNA_GenBank/${id}.fa \
-	-bed ${id}_transcribed.bed | bgzip > rDNA_transcribed.fa.gz
+	-bed ${id}_roi.bed | bgzip > rDNA_roi.fa.gz
--- 03_alignment/do_alignment.sh
+++ 03_alignment/do_alignment.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-bowtie2 \
-	-x ../01_bowtie2_ref/albacore_canu_wtdbg_nanopolish2 \
-	-U ../02_rDNA_frags/rDNA_frags_len500_step500.fa.gz \
-	-f \
-	--threads 4 \
-	--all \
-	2> ../logs/bowtie2_rDNA_frags_len500_step500.log | samtools view \
-	-bSh -@ 4 -F4 - > unsorted_rDNA_frags_len500_step500.bam
-bowtie2 \
-	-x ../01_bowtie2_ref/albacore_canu_wtdbg_nanopolish2 \
-	-U ../02_rDNA_frags/rDNA_transcribed.fa.gz \
-	-f \
-	--threads 4 \
-	--all \
-	2> ../logs/bowtie2_rDNA_transcribed.log | samtools view \
-	-bSh -@ 4 -F4 - > unsorted_rDNA_transcribed.bam
+id=("rDNA_frags_len500_step500" "rDNA_roi")
 
+for name in "${id[@]}"; do
 
-samtools sort -@ 4 \
-	-o rDNA_frags_len500_step500.bam unsorted_rDNA_frags_len500_step500.bam
-samtools sort -@ 4 \
-	-o rDNA_transcribed.bam unsorted_rDNA_transcribed.bam
-rm -f unsorted_rDNA_frags_len500_step500.bam
-rm -f unsorted_rDNA_transcribed.bam
+	bowtie2 \
+		-x ../01_bowtie2_ref/albacore_canu_wtdbg_nanopolish2 \
+		-U ../02_rDNA_frags/$name.fa.gz \
+		-f \
+		--threads 4 \
+		--all \
+		2> ../logs/bowtie2_$name.log | samtools view \
+		-bSh -@ 4 -F4 - > unsorted_$name.bam
 
+	samtools sort -@ 4 \
+		-o $name.bam unsorted_$name.bam
 
-samtools index rDNA_frags_len500_step500.bam
-samtools index rDNA_transcribed.bam
+	rm -f unsorted_$name.bam
 
+	samtools index $name.bam
 
-bedtools bamtobed -i rDNA_frags_len500_step500.bam > rDNA_frags_len500_step500.bed
-bedtools bamtobed -i rDNA_transcribed.bam > rDNA_transcribed.bed
+	bedtools bamtobed -i $name.bam > $name.bed
+
+done
